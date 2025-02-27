14b167cfd0da6350492fbdea4aff6fa9f9734243
Aadhi <adithyasekhark_bv2019@ug.cusat.ac.in>
2025-02-27 17:40:40 +0530

    Home Screen almost done, bottom nav left

---
+++ app/src/main/java/com/example/gitflow/navigation/BottomNavItem.kt
@@ -0,0 +1,60 @@
+package com.example.gitflow.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.gitflow.ui.cartscreen.CartScreen
+import com.example.gitflow.ui.favoritesscreen.FavoritesScreen
+import com.example.gitflow.ui.mainScreens.HomeScreen
+import com.example.gitflow.ui.profilescreen.ProfileScreen
+
+sealed class BottomNavItem(val route: String, val icon: ImageVector, val label: String) {
+    object Home : BottomNavItem("home", Icons.Default.Home, "Home")
+    object Favorites : BottomNavItem("favorites", Icons.Default.Favorite, "Favorites")
+    object Cart : BottomNavItem("cart", Icons.Default.ShoppingCart, "Cart")
+    object Profile : BottomNavItem("profile", Icons.Default.Person, "Profile")
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Favorites,
+        BottomNavItem.Cart,
+        BottomNavItem.Profile
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    NavigationBar {
+        items.forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(item.icon, contentDescription = item.label) },
+                label = { Text(item.label) },
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
